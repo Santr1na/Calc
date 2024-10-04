ea1bda0dd682a75621f643e62ff3e65d727a7b90
Santrina <f.klokov00@gmail.com>
2024-10-04 16:29:02 +0300

    first version

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, result;
+            Console.WriteLine("Введите первое число");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            b = Convert.ToInt32(Console.ReadLine());
+            result = a + b;
+            Console.WriteLine("Сумма = "+result);
+            Console.ReadKey();
+        }
+    }
+}
